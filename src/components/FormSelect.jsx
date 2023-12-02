5c307f2de435f7062d8584027c63b992c719d0d9
Biswajit Barik <nexenband@gmail.com>
2023-12-02 07:39:15 +0530

    FormSelect fixed

--- src/components/FormSelect.jsx
+++ src/components/FormSelect.jsx
@@ -1,31 +1,24 @@
-import { formatPrice } from "../utils";
-import { useState } from "react";
-const FormRange = ({ label, name, size, price }) => {
-  const step = 1000;
-  const maxPrice = 100000;
-  const [selectedPrice, setSelectedPrice] = useState(price || maxPrice);
-
+const FormSelect = ({ label, name, list, defaultValue, size }) => {
   return (
     <div className="form-control">
-      <label htmlFor={name} className="label cursor-pointer">
+      <label htmlFor={name} className="label">
         <span className="label-text capitalize">{label}</span>
-        <span>{formatPrice(selectedPrice)}</span>
       </label>
-      <input
-        type="range"
+      <select
         name={name}
-        min={0}
-        max={maxPrice}
-        value={selectedPrice}
-        onChange={(e) => setSelectedPrice(e.target.value)}
-        className={`range range-primary ${size}`}
-        step={step}
-      />
-      <div className="w-full flex justify-between text-xs px-2 mt-2">
-        <span className="font-bold text-md">0</span>
-        <span className="font-bold text-md">Max : {formatPrice(maxPrice)}</span>
-      </div>
+        id={name}
+        className={`select select-bordered ${size}`}
+        defaultValue={defaultValue}
+      >
+        {list.map((item) => {
+          return (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          );
+        })}
+      </select>
     </div>
   );
 };
-export default FormRange;
+export default FormSelect;
